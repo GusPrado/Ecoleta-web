53371078bd0fa1a933e624c4f7cee246631eaaa4
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-06-03 13:45:40 -0300

    Added homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 
 import './App.css';
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-          Learn React
-      </header>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
