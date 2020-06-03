3bf7922dd8b24f782627da8b2a29a49729079455
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-06-03 15:45:43 -0300

    Added map click to fix position feat.

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet'
 import axios from 'axios'
+import { LeafletMouseEvent } from 'leaflet'
 
 import './styles.css'
 
@@ -29,6 +30,7 @@ const CreatePoint = () => {
 
   const [selectedUf, setSeletectedUf] = useState('0')
   const [selectedCity, setSeletectedCity] = useState('0')
+  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0])
 
   useEffect(() => {
     api.get('items').then(response => {
@@ -71,6 +73,13 @@ const CreatePoint = () => {
     const city = event.target.value
 
     setSeletectedCity(city)
+  }
+
+  function handleMapClick(event: LeafletMouseEvent) {
+    setSelectedPosition([
+      event.latlng.lat,
+      event.latlng.lng
+    ])
   }
 
   return (
@@ -127,13 +136,13 @@ const CreatePoint = () => {
             <span>Selecione o endereço no mapa</span>
           </legend>
 
-          <Map center={[-27.5588003, -48.497478]} zoom={15}>
+          <Map center={[-27.5588003, -48.497478]} zoom={15} onClick={handleMapClick}>
             <TileLayer 
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
 
-            <Marker position={[-27.5588003, -48.497478]}/>
+            <Marker position={selectedPosition}/>
           </Map>
 
           <div className="field-group">
